b9a3b815b40f09dc42e9f27a3fcb055f59e18154
mettkov <che.mehdi.zarei@gmail.com>
2020-10-23 18:48:11 +0330

    Init Commit

---
+++ Assets/Scripts/Actors/JumperController.cs
@@ -0,0 +1,53 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace JigiJumper.Actors
+{
+    public class JumperController : MonoBehaviour
+    {
+        [SerializeField] private CinemachineVirtualCamera _cinemachine = null;
+        [SerializeField] private float _speed = 10f;
+        
+        PlanetController _planet;
+
+        private void Update()
+        {
+            HandleInput();
+
+            if (_planet != null)
+            {
+                transform.position = _planet.GetPivotCircuit().position;
+                transform.rotation = _planet.GetPivotCircuit().rotation;
+            }
+            else
+            {
+                transform.Translate(Vector2.up * (Time.deltaTime * _speed));
+            }
+        }
+
+        private void HandleInput()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                _planet = null;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            PlanetController planetController = collision.GetComponent<PlanetController>();
+            
+            if (planetController == null) { return; }
+
+            _planet = planetController;
+
+            Transform pivot = _planet.GetPivot();
+
+            float angle = Vector2.SignedAngle(pivot.transform.up, -transform.up);
+
+            pivot.localRotation *= Quaternion.Euler(Vector3.forward * angle);
+
+            _cinemachine.Follow = _planet.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/Actors/PlanetController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+namespace JigiJumper.Actors
+{
+    public class PlanetController : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed = 50f;
+
+        [SerializeField] private Transform _pivot = null;
+
+        Transform _circuit;
+
+        private void Awake()
+        {
+            _circuit = _pivot.GetChild(0);
+        }
+
+        void LateUpdate()
+        {
+            HandleRotation();
+        }
+
+        private void HandleRotation()
+        {
+            _pivot.Rotate(Vector3.forward * (Time.deltaTime * _rotationSpeed));
+        }
+
+        public Transform GetPivot()
+        {
+            return _pivot;
+        }
+
+        public Transform GetPivotCircuit()
+        {
+            return _circuit;
+        }
+    }
+}

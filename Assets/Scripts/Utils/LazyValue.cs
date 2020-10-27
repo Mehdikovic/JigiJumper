3582e289e50a1d1776a1c4ea53f2f40502995cb0
mettkov <che.mehdi.zarei@gmail.com>
2020-10-27 15:22:35 +0330

    adding Lazy Initialization

---
+++ Assets/Scripts/Utils/LazyValue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace JigiJumper.Utils
+{
+    public class LazyValue<T>
+    {
+        private T _value;
+        private bool _initialized = false;
+        private Func<T> _initializer;
+
+        public LazyValue(Func<T> initializer)
+        {
+            _initializer = initializer;
+        }
+        
+        public T value
+        {
+            get
+            {
+                ForceInit();
+                return _value;
+            }
+            private set
+            {
+                _initialized = true;
+                _value = value;
+            }
+        }
+
+        public void ForceInit()
+        {
+            if (!_initialized)
+            {
+                _value = _initializer();
+                _initialized = true;
+            }
+        }
+    }
+}

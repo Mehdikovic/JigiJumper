e2deb79de75d4bf469f3c733d69d09b1e27e5ad4
mettkov <che.mehdi.zarei@gmail.com>
2020-11-03 18:21:43 +0330

    small visual improvements

---
+++ Assets/Scripts/Component/GfxTintChanger.cs
@@ -0,0 +1,49 @@
+﻿using JigiJumper.Actors;
+using JigiJumper.Data;
+using JigiJumper.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JigiJumper.Component
+{
+
+    public class GfxTintChanger : MonoBehaviour
+    {
+        private const int MAX_RANGE = 1000000;
+        
+        [SerializeField] SpriteRenderer _spriteRenderer = null;
+        [SerializeField] Color[] _colors = new Color[2];
+
+        private Queue<Color> _colorQueue = null;
+        private int _seed;
+        private PlanetController _planetController;
+
+        private void Awake()
+        {
+            _seed = Random.Range(0, MAX_RANGE);
+            _colorQueue = new Queue<Color>(Utility.Shuffle(_colors, _seed));
+            _planetController = GetComponent<PlanetController>();
+
+            _planetController.OnSpawnedInitialization += ChangeColor;
+        }
+
+        void ChangeColor(PlanetDataStructure data)
+        {
+           
+            Color color;
+            if (_colorQueue.Count > 0)
+            {
+                color = _colorQueue.Dequeue();
+            }
+            else
+            {
+                _colorQueue = new Queue<Color>(Utility.Shuffle(_colors, ++_seed % MAX_RANGE));
+                color = _colorQueue.Dequeue();
+            }
+
+            _spriteRenderer.color = color;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Utils/Utility.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+namespace JigiJumper.Utils
+{
+    public class Utility
+    {
+        static public T[] Shuffle<T>(T[] array, int seed)
+        {
+            System.Random rand = new System.Random(seed);
+            for (int i = 0; i < array.Length - 1; ++i)
+            {
+                int randomIndex = rand.Next(i, array.Length - 1);
+                T temp = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = temp;
+            }
+            return array;
+        }
+
+        static public float Map(float value, float fromMin, float fromMax, float toMin, float toMax)
+        {
+            return (value - fromMin) / (fromMax - fromMin) * (toMax - toMin) + toMin;
+        }
+
+        static public float Interpolate(float percent)
+        {
+            return 4 * (-Mathf.Pow(percent, 2) + percent);
+        }
+    }
+}

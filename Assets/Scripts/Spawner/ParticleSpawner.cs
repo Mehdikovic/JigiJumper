9438c7f3b50f8711a299e09ba071ea4d334b0e70
mettkov <che.mehdi.zarei@gmail.com>
2020-11-05 23:52:51 +0330

    adding some vfxs, particle and trail to jumper and planet destruction

---
+++ Assets/Scripts/Spawner/ParticleSpawner.cs
@@ -0,0 +1,23 @@
+﻿using JigiJumper.Managers;
+using UnityEngine;
+
+
+namespace JigiJumper.Spawner
+{
+    public class ParticleSpawner : MonoBehaviour
+    {
+
+        [SerializeField] ParticleSystem _particlePrefab = null;
+        
+        void Awake()
+        {
+            GameManager.Instance.jumper.OnPlanetReached += OnPlanetReached;
+        }
+
+        private void OnPlanetReached(Actors.PlanetController oldPlanet, Actors.PlanetController newPlanet)
+        {
+            if (oldPlanet != null)
+                Instantiate(_particlePrefab, oldPlanet.transform.position, Quaternion.identity);
+        }
+    }
+}

636f149443762fdf628053307565a57ed2e7c789
mettkov <che.mehdi.zarei@gmail.com>
2020-11-05 16:52:43 +0330

    adding level increasing and its UI component, adding text mesh pro package

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -9,6 +9,7 @@ namespace JigiJumper.Managers
 {
     public class GameManager : MonoBehaviour
     {
+        private const int LEVEL_DETR = 5;
         #region Singleton
         static GameManager _instance;
         public static GameManager Instance => GetInstance();
@@ -22,22 +23,27 @@ namespace JigiJumper.Managers
         }
         #endregion
 
-        [SerializeField] private SpawnProbabilities[] _spwanProbabilities = null;
-        [SerializeField] private int index = 0; // todo remove me!
+        [SerializeField] private SpawnProbabilities[] _spawnProbabilities = null;
 
         LazyValue<JumperController> _lazyJumper = new LazyValue<JumperController>( () => FindObjectOfType<JumperController>());
         JumperController _jumper;
 
+        private int _point = 0;
+       
+        public event Action<int> OnLevelChanged;
+        
         private void Awake()
         {
             _jumper = _lazyJumper.value;
+            _jumper.OnPlanetReached += OnPlanetReached;
         }
 
         public JumperController jumper => _lazyJumper.value;
 
         public SpawnProbabilities GetSpawnProbabilities()
         {
-            return _spwanProbabilities[index];
+            int validIndex = Mathf.Clamp(GetLevel() - 1, 0, _spawnProbabilities.Length - 1);
+            return _spawnProbabilities[validIndex];
         }
 
         public void RequestSelfDestructionPlanet(GameObject selfDestructorGameObject)
@@ -45,6 +51,20 @@ namespace JigiJumper.Managers
             if (_jumper.currentPlanetGameObject != selfDestructorGameObject) { return; }
 
             print("destroyed");
+        }
+
+        private void OnPlanetReached(Actors.PlanetController arg1, Actors.PlanetController arg2)
+        {
+            ++_point;
+
+            if (_point % LEVEL_DETR != 0) { return; }
+
+            OnLevelChanged?.Invoke(GetLevel());
+        }
+
+        private int GetLevel()
+        {
+            return Mathf.Clamp(_point / LEVEL_DETR + 1, 1, 999);
         }
     }
 }
---
+++ Assets/Scripts/UI/LevelUI.cs
@@ -0,0 +1,52 @@
+﻿using JigiJumper.Managers;
+using TMPro;
+using UnityEngine;
+
+
+namespace JigiJumper.UI
+{
+    public class LevelUI : MonoBehaviour
+    {
+        private const int REMAINING_PLANET = 3;
+        
+        [SerializeField] private GameObject[] _actives = null;
+        [SerializeField] private TextMeshProUGUI _textPro = null;
+
+        int _planetsRemainings = REMAINING_PLANET;
+
+        private void Awake()
+        {
+            _textPro.text = "1";
+
+            GameManager.Instance.OnLevelChanged += OnLevelChanged;
+            GameManager.Instance.jumper.OnPlanetReached += OnPlanetReached;
+            
+        }
+
+        private void OnPlanetReached(Actors.PlanetController arg1, Actors.PlanetController arg2)
+        {
+            --_planetsRemainings;
+
+            if (_planetsRemainings <= 0)
+            {
+                _planetsRemainings = 0;
+                SetActiveOfGameObjects(false);
+            }
+        }
+
+        private void OnLevelChanged(int newLevel)
+        {
+            _planetsRemainings = REMAINING_PLANET;
+            SetActiveOfGameObjects(true);
+            _textPro.text = newLevel.ToString();
+        }
+
+        private void SetActiveOfGameObjects(bool value)
+        {
+            foreach (var go in _actives)
+            {
+                go.SetActive(value);
+            }
+        }
+    }
+}

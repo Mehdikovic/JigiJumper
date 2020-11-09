59397e35810527f3fe4ed0cae373ef7b95e51d4c
mettkov <che.mehdi.zarei@gmail.com>
2020-11-09 18:20:26 +0330

    adding trail particle system to jumper

---
+++ Assets/Scripts/Component/PlanetParticleEngineController.cs
@@ -0,0 +1,34 @@
+﻿using JigiJumper.Actors;
+using UnityEngine;
+
+
+namespace JigiJumper.Component
+{
+    public class PlanetParticleEngineController : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem _particleInstance = null;
+        [SerializeField] JumperController _jumper = null;
+
+        private void Awake()
+        {
+            _jumper.OnJump += OnJumperJump;
+            _jumper.OnPlanetReached += OnPlanetReached;
+            _jumper.OnRestart += OnJumperRestart;
+        }
+
+        private void OnJumperRestart()
+        {
+            _particleInstance.Stop();
+        }
+
+        private void OnPlanetReached(PlanetController arg1, PlanetController arg2)
+        {
+            _particleInstance.Stop();
+        }
+
+        private void OnJumperJump()
+        {
+            _particleInstance.Play();
+        }
+    }
+}

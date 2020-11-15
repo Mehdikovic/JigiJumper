06ce3aa5e722a67f3627192ae648e21970984ee8
mettkov <che.mehdi.zarei@gmail.com>
2020-11-15 19:21:29 +0330

    refactoring names of Camera vfx component

--- Assets/Scripts/Component/CameraVFX.cs
+++ Assets/Scripts/Component/CameraVFX.cs
@@ -7,12 +7,18 @@ namespace JigiJumper.Component
 
     public class CameraVFX : MonoBehaviour
     {
-        private const float S = 43 / 100f;
-        private const float V = 30 / 100f;
-        private const float HRange = 360f;
+        private const float HSV_S_MAX = 100f;
+        private const float HSV_V_MAX = 100f;
+        private const float HSV_H_MAX = 360f;
 
         [SerializeField] private Camera _camera = null;
-        [SerializeField] private float _timer = 0.1f;
+        
+        [Header("Color HSV")]
+        [SerializeField] private int HSV_s = 43;
+        [SerializeField] private int HSV_v = 30;
+        
+        [Header("Time between change")]
+        [SerializeField] private float _timer = 0.05f;
 
         WaitForSeconds _wait;
 
@@ -30,7 +36,13 @@ namespace JigiJumper.Component
             while (true)
             {
                 H = increase ? ++H : --H;
-                _camera.backgroundColor = Color.HSVToRGB(H / HRange, S, V); ;
+                
+                _camera.backgroundColor = Color.HSVToRGB(
+                    H / HSV_H_MAX,
+                    HSV_s / HSV_S_MAX,
+                    HSV_v / HSV_V_MAX
+                );
+                
                 yield return _wait;
                 
                 if (H == 320) 

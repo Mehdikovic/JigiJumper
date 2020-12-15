ca2c5173066c4eb0d8f24daf648ba2389e4ac0b2
mettkov <che.mehdi.zarei@gmail.com>
2020-12-15 20:25:48 +0330

    refactoring DoTween utilities at Show/Hide Window

--- Assets/Scripts/UI/Ui.cs
+++ Assets/Scripts/UI/Ui.cs
@@ -9,31 +9,33 @@ namespace JigiJumper.Ui {
             container.gameObject.SetActive(true);
             container.localScale = new Vector3(0, 0, 1);
 
-            container
-                .DOScaleX(1.3f, .3f)
-                .onComplete = () => container.DOScaleX(1f, .1f);
-
-            container
-                .DOScaleY(1.3f, .3f)
-                .onComplete = () => container.DOScaleY(1f, .1f)
-                .onComplete = () => onComplete?.Invoke();
+            Sequence initial = DOTween.Sequence();
+            initial.onComplete = () => {
+                Sequence second = DOTween.Sequence();
+                second.onComplete = () => onComplete?.Invoke();
+                second.Join(container.DOScaleX(1f, .1f));
+                second.Join(container.DOScaleY(1f, .1f));
+            };
+            initial.Join(container.DOScaleX(1.3f, 0.3f));
+            initial.Join(container.DOScaleY(1.3f, 0.3f));
         }
 
         public static void DoHideWindow(RectTransform container, Action onComplete = null) {
             container.localScale = new Vector3(1, 1, 1);
 
-            container
-                .DOScaleX(1.2f, .1f)
-                .onComplete = () => container.DOScaleX(0f, .3f);
-
-            container
-                .DOScaleY(1.2f, .1f)
-                .onComplete = () => container.DOScaleY(0f, .3f)
-                .onComplete = () => {
+            Sequence initial = DOTween.Sequence();
+            initial.onComplete = () => {
+                Sequence second = DOTween.Sequence();
+                second.onComplete = () => {
                     onComplete?.Invoke();
                     container.localScale = new Vector3(0, 0, 1);
                     container.gameObject.SetActive(false);
                 };
+                second.Join(container.DOScaleX(0f, .3f));
+                second.Join(container.DOScaleY(0f, .3f));
+            };
+            initial.Join(container.DOScaleX(1.2f, 0.1f));
+            initial.Join(container.DOScaleY(1.2f, 0.1f));
         }
 
         static public void SetBehaviorActivation(bool active, IEnumerable<Behaviour> behaviors) {

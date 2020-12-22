287626536e374ea6f5f503cd9d072897a3ba40ee
mettkov <che.mehdi.zarei@gmail.com>
2020-12-22 14:28:15 +0330

    adding timer tick sound

--- Assets/Scripts/Component/SelfDestructor.cs
+++ Assets/Scripts/Component/SelfDestructor.cs
@@ -4,7 +4,6 @@ using JigiJumper.Data;
 using JigiJumper.Managers;
 using JigiJumper.Actors;
 
-
 namespace JigiJumper.Component {
     public class SelfDestructor : MonoBehaviour {
         [SerializeField] private Transform _pivot = null;
@@ -16,11 +15,18 @@ namespace JigiJumper.Component {
         private float _storedRotationSpeed;
         private float _timer;
         private bool _isActivated;
+        bool _isTimerStart = false;
         private bool _isFirstPlanet = true;
         private DestructionState _internalState = DestructionState.None;
+        private AudioSource _audio;
+        private WaitForSeconds _wait;
 
         private void Awake() {
             _gameManager = GameManager.instance;
+            _audio = GetComponent<AudioSource>();
+            _audio.loop = false;
+            _audio.playOnAwake = false;
+            _wait = new WaitForSeconds(1f);
 
             PlanetController planetController = GetComponent<PlanetController>();
 
@@ -48,12 +54,18 @@ namespace JigiJumper.Component {
             if (_isActivated == false) { return; }
 
             _timer -= Time.deltaTime;
+
+            TimeToStartTimer(_timer);
+
+            //PlayTimerSound(_timer);
             if (_timer <= 0f) {
                 _timer = 0f;
                 _gameManager.RequestSelfDestructionPlanet(gameObject);
                 _isActivated = false;
             }
         }
+
+
 
         private void LateUpdate() {
             HandleRotation();
@@ -97,6 +109,27 @@ namespace JigiJumper.Component {
         public void OnJumperExit() {
             _isActivated = false;
             _rotationSpeed = _storedRotationSpeed; // because of restart system we need to store previous value of rotation
+            _isTimerStart = false;
+            _audio.Stop();
+            StopAllCoroutines();
+        }
+
+
+        private void TimeToStartTimer(float timer) {
+            if (_isTimerStart) { return; }
+            if (Mathf.FloorToInt(timer) == 3) {
+                _isTimerStart = true;
+                StartCoroutine(PlayTimer(timer - 3f));
+            }
+        }
+
+        System.Collections.IEnumerator PlayTimer(float wait) {
+            yield return new WaitForSeconds(wait);
+            _audio.Play();
+            yield return _wait;
+            _audio.Play();
+            yield return _wait;
+            _audio.Play();
         }
     }
 }
